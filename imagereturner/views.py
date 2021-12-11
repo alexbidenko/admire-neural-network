3b1129899a4ac59e821d51f42f7d2723a90b586f
grakky <akimgrebenkin21@gmail.com>
2021-12-11 14:50:55 +0300

    delete old files

--- imagereturner/views.py
+++ imagereturner/views.py
@@ -6,6 +6,8 @@ import random
 import requests
 import json
 import os
+import platform
+import time
 
 access_token = "94b8c4c694b8c4c694b8c4c68894c2ab6e994b894b8c4c6f50b01863e04926b911fb650"
 
@@ -26,6 +28,7 @@ def resizer(image_name):
     pil_image = Image.open(image_name).resize((1000, 1000)).convert('RGB')
     return pil_image
 
+
 def get_label(phrase):
     font_size = random.randint(100, 300)
     random_font_file = random.choice(os.listdir("imagereturner/fonts"))
@@ -38,8 +41,32 @@ def get_label(phrase):
     return [label, string_size_in_pixels]
 
 
+def creation_date(path_to_file):
+    if platform.system() == 'Windows':
+        return os.path.getctime(path_to_file)
+    else:
+        stat = os.stat(path_to_file)
+        try:
+            return stat.st_birthtime
+        except AttributeError:
+            # We're probably on Linux. No easy way to get creation dates here,
+            # so we'll settle for when its content was last modified.
+            return stat.st_mtime
+
+
+def delete_old_files(filename):
+    file_path = "trash/" + filename
+    date_in_ms = creation_date(file_path)
+    if int(time.time()) -date_in_ms > 60:
+        os.remove(file_path)
+    return date_in_ms
+
+
 def return_image(request):
     if request.method == "POST":
+
+        downloaded_files = os.listdir("trash")
+        list(map(delete_old_files, downloaded_files))
         request = json.loads(request.body)
         phrase = request["phrase"]
         print(phrase)
